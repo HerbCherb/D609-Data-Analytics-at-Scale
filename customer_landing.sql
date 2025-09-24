25e35f9e1f4fff4429c16d64cc6d007a94a56949
HerbCherb <167220516+HerbCherb@users.noreply.github.com>
2025-09-24 13:02:40 -0400

    Create customer_landing.sql

---
+++ customer_landing.sql
@@ -0,0 +1,23 @@
+CREATE EXTERNAL TABLE `customer_landing`(
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `phone` string COMMENT 'from deserializer', 
+  `birthday` string COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `registrationdate` bigint COMMENT 'from deserializer', 
+  `lastupdatedate` bigint COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithfriendsasofdate` bigint COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://jonwgu/customer/landing/'
+TBLPROPERTIES (
+  'CreatedByJob'='Spark_job', 
+  'CreatedByJobRun'='jr_cb09585677330d385a7854bd71bb33d209f18142aaec73aec9eeb2714c0f8526', 
+  'classification'='json')
