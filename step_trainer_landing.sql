ebb05937d73370378bc54dc47cea08f0608eb29e
HerbCherb <167220516+HerbCherb@users.noreply.github.com>
2025-09-24 14:19:01 -0400

    Update step_trainer_landing.sql

--- step_trainer_landing.sql
+++ step_trainer_landing.sql
@@ -1,15 +1,16 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS `jonwgu`.`step_trainer_landing` (
-  `sensorReadingTime` bigint,
-  `serialNumber` string,
-  `distanceFromObject` bigint
-)
-ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-WITH SERDEPROPERTIES (
-  'ignore.malformed.json' = 'FALSE',
-  'dots.in.keys' = 'FALSE',
-  'case.insensitive' = 'TRUE',
-  'mapping' = 'TRUE'
-)
-STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://jonwgu/step_trainer/landing/'
-TBLPROPERTIES ('classification' = 'json');
+CREATE EXTERNAL TABLE `step_trainer_landing`(
+  `sensorreadingtime` bigint COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `distancefromobject` int COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'ignore.malformed.json'='true') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
+LOCATION
+  's3://jonwgu/step_trainer/landing'
+TBLPROPERTIES (
+  'has_encrypted_data'='false')
